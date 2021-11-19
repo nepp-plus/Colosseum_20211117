662961a2705ad80827fa7eb5f6377b31cf407729
nepp-plus <nepp.plus@gmail.com>
2021-11-19 16:18:41 +0900

    실제 싫어요 API 호출  - 응답 처리 X, 갯수 변화도 없다.

--- app/src/main/java/com/neppplus/colosseum_20211117/adapters/ReplyAdapter.kt
+++ app/src/main/java/com/neppplus/colosseum_20211117/adapters/ReplyAdapter.kt
@@ -74,6 +74,13 @@ class ReplyAdapter(
 
 //            이 댓글에 싫어요를 남겼다고 -> 서버 API 호출.
 
+            ServerUtil.postRequestReplyLikeOrDislike(mContext, data.id, false, object : ServerUtil.JsonResponseHandler {
+                override fun onResponse(jsonObj: JSONObject) {
+
+                }
+
+            })
+
         }
 
         return row
